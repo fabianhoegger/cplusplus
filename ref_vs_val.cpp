5430f59b130c779d9ca6efc8c895306567916d54
Fabian <draizfabian@gmail.com>
2021-01-16 19:18:43 +0200

    code example for by reference

---
+++ ref_vs_val.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+void swap_by_val(int x,int y){
+    int temp=x;
+    x=y;
+    y=temp;
+
+}
+int main()
+{
+    int var1=1;
+    int var2=2;
+
+}
