de02587e607a685e8528bc192d3409e011c469e1
Fabian <draizfabian@gmail.com>
2021-01-17 23:41:50 +0200

    class

--- Aggregator.cpp
+++ Aggregator.cpp
@@ -1,3 +1,14 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
 class Aggregator {
   int *numbers_arr;
   int numbers_size;
@@ -20,4 +31,30 @@ Aggregator::Aggregator(int n) {
   //deconstructor
   delete[] numbers_arr;
   cout << "Aggregator size " << numbers_size << " cleaned up." << endl;
+}
+void Aggregator::add_number(int i) {
+  if ( next_number < numbers_size) {
+     numbers_arr[next_number++] = i;
+       }
+  else
+      cout << "Aggregator already full.";
+ }
+ int Aggregator::sum_all() {
+  int sum=0;
+  for ( int i=0; i<numbers_size; i++){
+     sum += numbers_arr[i];
+   }
+  return sum;
+ }
+int main() {
+  int nums;
+  cout << "Πόσουςαριθμούςθααθροίσετε;\n" ;
+  cin >> nums;
+  Aggregator agg = Aggregator(nums);
+  int in;
+  for ( int i=0; i<nums; i++) {
+  cout << "Αριθμός " << (i+1) << "ος: " ;
+  cin >> in; agg.add_number(in);
+  }
+  cout << "Άθροισμα= " << agg.sum_all();
 }
